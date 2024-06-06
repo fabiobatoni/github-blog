47191d3c36f77b5890d40d3726eab2a0a5254689
fabiobatoni <fabio_batoni@hotmail.com>
2024-06-06 11:41:39 -0300

    component: setup: create style dependencies
    - Install styled-components.
    - Defines default style variables
    - Create page Home
    - Apply ThemeProvider
    
    Signed-off-by: fabiobatoni <fabio_batoni@hotmail.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,15 @@
+import { ThemeProvider } from "styled-components";
+import { defaultTheme } from "./styles/themes/default";
+import { GlobalStyle } from "./styles/global";
+import { Home } from "./pages/Home";
+
+
 export function App() {
   return (
-    <h1>Github Blog</h1>
+    <ThemeProvider theme={defaultTheme}>
+      <GlobalStyle />
+      <Home />
+  </ThemeProvider>
   )
 }
 
---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,5 @@
+export function Home() {
+  return(
+    <h1>GitHub Blog</h1>
+  )
+}
