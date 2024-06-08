ec980df99328f0983effb509861335d8807b410c
fabiobatoni <fabio_batoni@hotmail.com>
2024-06-08 11:41:19 -0300

    component: header: create Header
    Create Header Component
    
    Signed-off-by: fabiobatoni <fabio_batoni@hotmail.com>

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,10 @@
+import imgHeader from '../../assets/header.svg'
+
+export function Header() {
+  return(
+    <img 
+      src={imgHeader} 
+      alt="fundo header" 
+    />
+  )
+}
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,5 +1,7 @@
+import { Header } from "../../components/Header";
+
 export function Home() {
   return(
-    <h1>GitHub Blog</h1>
+    <Header />
   )
 }
