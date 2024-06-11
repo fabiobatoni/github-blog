a881b87f48fdbd4f7a8cc844cc78750f834d1f88
fabiobatoni <fabio_batoni@hotmail.com>
2024-06-11 10:57:54 -0300

    component: Details: add DetailsContent
    Add DetailsContent styled component
    
    Signed-off-by: fabiobatoni <fabio_batoni@hotmail.com>

--- src/pages/Details/index.tsx
+++ src/pages/Details/index.tsx
@@ -1,6 +1,6 @@
 import { Header } from "../../components/Header";
 import { DetailsInfo } from "./DetailsInfo";
-import { DetailsContainer } from "./styles";
+import { DetailsContainer, DetailsContent } from "./styles";
 
 export function Details() {
   return(
@@ -8,6 +8,17 @@ export function Details() {
       <Header />
       <DetailsContainer>
         <DetailsInfo />
+
+        <DetailsContent>
+          <div>
+          Programming languages all have built-in data structures, but these often differ from one language to another. This article attempts to list the built-in data structures available in JavaScript and what properties they have. These can be used to build other data structures. Wherever possible, comparisons with other languages are drawn.
+
+Dynamic typing
+JavaScript is a loosely typed and dynamic language. Variables in JavaScript are not directly associated with any particular value type, and any variable can be assigned (and re-assigned) values of all types:
+
+          </div>
+        </DetailsContent>
+
       </DetailsContainer>
     </>
   )
